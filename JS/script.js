4052872f6b339fabf60a64265befa236226a067a
abhijit <rangerrkyt@gmail.com>
2023-12-04 17:11:12 +0530

    add css and modefy booking page

--- JS/script.js
+++ JS/script.js
@@ -138,6 +138,16 @@ jQuery(document).ready(function () {
   });
 });
 
+// const toggleBtn = document.querySelector('.toggle-btn')
+// const toggleBtnIcon = document.querySelector('.toggle-btn i')
+// const dropDownMenu = document.querySelector('.dropdown-menu')
 
 
+// toggleBtn.oneclick = function () {
+//   dropDownMenu.classList.toggle('open')
+//   const isOpen = dropDownMenu.classList.contains('open')
 
+//   toggleBtnIcon.classList = isOpen
+//   ? 'fa-solid fa-xmark'
+//   : 'fa-solid fa-bars'
+// }
